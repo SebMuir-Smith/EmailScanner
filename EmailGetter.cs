de0e4d5fb9f90451c40b02ee536b30846f345d43
Sebastian Muir-Smith <sebastian@muirsmith.com>
2019-06-13 15:11:20 +1000

    initial commit

---
+++ EmailGetter.cs
@@ -0,0 +1,9 @@
+namespace EmailScanner
+{
+    public class EmailGetter
+    {
+        public string userName;
+        
+        public string password;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace EmailScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
