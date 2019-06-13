9ca32b246195939db2d66611626c29e73afa0c04
Sebastian Muir-Smith <sebastian@muirsmith.com>
2019-06-13 16:33:43 +1000

    Add working query object contructor

--- EmailInfo.cs
+++ EmailInfo.cs
@@ -1,24 +1,29 @@
 using System.Collections.Generic;
 using MailKit.Net.Imap;
-using MimeKit;
+using MailKit;
+using MailKit.Search;
+using System.IO;
 namespace EmailScanner
 {
     public class EmailInfo
     {
         public string userName;
-        
+
         public string password;
 
         public string serverName;
 
         public int port;
 
+        private SearchQuery searchForErrors;
+
 
         /// <summary>
         /// Constructor for email info object
         /// </summary>
         /// <param name="args">string[4] containing name, password, server and port</param>
-        public EmailInfo(string[] args){
+        public EmailInfo(string[] args)
+        {
 
             userName = args[0];
 
@@ -28,25 +33,49 @@ namespace EmailScanner
 
             port = int.Parse(args[3]);
 
+            searchForErrors = GetSearchQuery();
+
         }
-        /// <summary>
-        /// Returns a list of Emails using given info
-        /// </summary>
-        public List<dynamic> GetEmails(){
 
-            using (ImapClient connection = new ImapClient()){
+        private SearchQuery GetSearchQuery()
+        {
+            string[] queries = File.ReadAllLines("queries.txt");
 
-                connection.Connect(this.serverName,this.port);
+            SearchQuery search = SearchQuery.BodyContains(queries[0]);
 
-                connection.Authenticate(this.userName, this.password);
-
-
-
+            for (int i = 1; i < queries.Length; i++){
+                search = search.Or(SearchQuery.BodyContains(queries[i]));
             }
-            
-            return new List<dynamic>();
+
+            return search;
         }
 
 
+    /// <summary>
+    /// Returns a list of Emails using given info
+    /// </summary>
+    public List<dynamic> GetEmails()
+    {
+
+        using (ImapClient connection = new ImapClient())
+        {
+
+            // Connect to server
+            connection.Connect(this.serverName, this.port);
+
+            connection.Authenticate(this.userName, this.password);
+
+            IMailFolder inbox = connection.Inbox;
+
+            inbox.Open(FolderAccess.ReadOnly);
+
+
+
+        }
+
+        return new List<dynamic>();
     }
+
+
+}
 }
